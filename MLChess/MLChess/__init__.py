f51f1c9da99acf62b429f478bb627eef1e8c9588
Kiogre <marcochiorri2003@gmail.com>
2025-12-24 18:54:51 +0100

    Finished making the library, tested and work with model that choose move and gives value

--- MLChess/MLChess/__init__.py
+++ MLChess/MLChess/__init__.py
@@ -1,9 +1,9 @@
 from .Representation.data_organization_tensor import create_dataloaders_tensor
 from .Models.my_resnet import MyResNet, NotUntilEndResNet
 from .Representation.graph_representation import dataset_creation_graph
-from .Models.graph_models import ChessGCN, IncompleteChessGCN
+from .Models.graph_models import ChessGCN, PoolingChessGCN, GraphAndPoolingChessGCN
 
-__version__ = "0.2.0"
+__version__ = "0.2.4"
 
 __all__ = ["create_dataloaders_tensor", "MyResNet", "NotUntilEndResNet", "dataset_creation_graph",
-           "ChessGCN", "IncompleteChessGCN"]
+           "ChessGCN", "PoolingChessGCN", "GraphAndPoolingChessGCN"]
--- MLChess/build/lib/MLChess/__init__.py
+++ MLChess/build/lib/MLChess/__init__.py
@@ -1,7 +1,9 @@
-from .data_organization_tensor import create_dataloaders_tensor
-from .my_resnet import MyResNet, NotUntilEndResNet
-from .graph_representation import dataset_creation_graph
+from .Representation.data_organization_tensor import create_dataloaders_tensor
+from .Models.my_resnet import MyResNet, NotUntilEndResNet
+from .Representation.graph_representation import dataset_creation_graph
+from .Models.graph_models import ChessGCN, PoolingChessGCN, GraphAndPoolingChessGCN
 
-__version__ = "0.1.5"
+__version__ = "0.2.4"
 
-__all__ = ["create_dataloaders_tensor", "MyResNet", "NotUntilEndResNet", "dataset_creation_graph"]
+__all__ = ["create_dataloaders_tensor", "MyResNet", "NotUntilEndResNet", "dataset_creation_graph",
+           "ChessGCN", "PoolingChessGCN", "GraphAndPoolingChessGCN"]
